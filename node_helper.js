889a68ad1d22223c1ec05987ea5d3ba61f4262d0
HeikoGr <20295490+HeikoGr@users.noreply.github.com>
2025-01-02 10:34:37 +0100

    add exam functionality, major redesign

--- node_helper.js
+++ node_helper.js
@@ -6,148 +6,171 @@ const Authenticator = require('otplib').authenticator;
 const Log = require("logger");
 
 module.exports = NodeHelper.create({
-		start: function () {
-	},
-	socketNotificationReceived: function (notification, payload) {
+    // Start function is called when the helper is initialized
+    start: function () {
+        Log.info("[MMM-Webuntis] Node helper started");
+    },
 
-		if (notification === "FETCH_DATA") {
+    // Function to handle socket notifications
+    socketNotificationReceived: async function (notification, payload) {
+        if (notification === "FETCH_DATA") {
+            this.config = payload;
 
-			//Copy and save config
-			this.config = payload;
+            try {
+                // every student needs its own Untis instance
+                for (const student of this.config.students) {
+                    let untis;
 
-			// iterate through students, fetch and send lessons
-			for (let i in this.config.students) {
-				var student = this.config.students[i];
-				this.fetchLessons(student, this.config.days);
-			}
-		}
-	},
+                    let identifier = this.config.id;
+                    let days = this.config.days;
+                    let examsDays = this.config.examsDays;
+                    let debugLastDays = this.config.debugLastDays;
 
-	fetchLessons: function (studentData, days) {
+                    if (student.qrcode) {
+                        // Create a WebUntisQR instance if QR code is provided
+                        untis = new WebUntisQR(student.qrcode, 'custom-identity', Authenticator, URL);
+                    } else if (student.username) {
+                        // Create a WebUntis instance if username and password are provided
+                        untis = new WebUntis(student.school, student.username, student.password, student.server);
+                    }
 
-		// create lessons array to be sent to module
-		var lessons = [];
+                    untis.login()
+                        .then(() => this.fetchData(untis, student, days, debugLastDays, identifier))
+                        .then(() => untis.logout())
+                        .catch(error => {
+                            console.error("[MMM-Webuntis] Error:", error);
+                        });
+                }
+                Log.info("[MMM-Webuntis] Successfully fetched data");
+            } catch (error) {
+                Log.error("[MMM-Webuntis] Error loading Untis data: ", error);
+            }
+        }
+    },
 
-		// array to get lesson number by start time
-		var startTimes = [];
+    fetchData: async function (untis, student, days, debugLastDays, identifier) {
 
-		let untis;
+        var lessons = [];
+        var exams = [];
+        var startTimes = [];
 
-		if (studentData.qrcode) {
-			untis = new WebUntisQR(studentData.qrcode, 'custom-identity', Authenticator, URL);
-		}
-		else if (studentData.username) {
-			untis = new WebUntis(studentData.school, studentData.username, studentData.password, studentData.server);
-		}
-		else if (studentData.class) {
-			untis = new WebUntis.WebUntisAnonymousAuth(studentData.school, studentData.server);
-		}
-		else {
-			console.log("Error: Student '" + studentData.title + "' has an configuration error!");
-			return;
-		}
+        // Validate the number of days
+        if (days < 1 || days > 10 || isNaN(days)) { days = 1; }
 
-		if (days < 1 || days > 10 || isNaN(days)) { days = 1; }
+        var rangeStart = new Date(Date.now());
+        var rangeEnd = new Date(Date.now());
+        rangeStart.setDate(rangeStart.getDate() - debugLastDays);
+        rangeEnd.setDate(rangeStart.getDate() + days);
 
-		untis
-			.login()
-			.then(response => {
-				var rangeStart = new Date(Date.now());
-				var rangeEnd = new Date(Date.now());
-				rangeEnd.setDate(rangeStart.getDate() + days);
+        try {
+            let timetable;
 
-				untis.getTimegrid()
-					.then(grid => {
-						// use grid of first day and assume all days are the same
-						grid[0].timeUnits.forEach(element => {
-							startTimes[element.startTime] = element.name;
-						})
+            untis.getTimegrid()
+                .then(grid => {
+                    // use grid of first day and assume all days are the same
+                    // used to get the numbers of the time units
+                    grid[0].timeUnits.forEach(element => { startTimes[element.startTime] = element.name; })
+                })
+                .catch(error => {
+                    console.log("Error in getTimegrid: " + error);
+                })
 
-					})
-					.catch(error => {
-						console.log("Error in getTimegrid: " + error);
-					})
-				if (studentData.useClassTimetable) {
-					return untis.getOwnClassTimetableForRange(rangeStart, rangeEnd);
-				} else {
-					return untis.getOwnTimetableForRange(rangeStart, rangeEnd);
-				}
-			})
-			.then(timetable => {
-				lessons = this.timetableToLessons(startTimes, timetable);
-				this.sendSocketNotification("GOT_DATA", { title: studentData.title, lessons: lessons });
-			})
-			.catch(error => {
-				console.log("ERROR for " + studentData.title + ": " + error.toString());
-			});
+            if (student.useClassTimetable) {
+                timetable = await untis.getOwnClassTimetableForRange(rangeStart, rangeEnd);
+            } else {
+                timetable = await untis.getOwnTimetableForRange(rangeStart, rangeEnd);
+            }
+            lessons = this.timetableToLessons(startTimes, timetable);
 
-		untis.logout();
-	},
+        } catch (error) {
+            console.log("[MMM-Webuntis] ERROR for " + student.title + ": " + error.toString());
+        }
 
-	timetableToLessons: function (startTimes, timetable) {
-		var lessons = [];
-		timetable.forEach(element => {
-			let lesson = {};
+        
+        if (student.fetchExams) {
 
-			// Parse date and time information
-			lesson.year = element.date.toString().substring(0, 4);
-			lesson.month = element.date.toString().substring(4, 6);
-			lesson.day = element.date.toString().substring(6);
-			lesson.hour = element.startTime.toString();
-			lesson.hour = (lesson.hour.length == 3 ? ("0" + lesson.hour.substring(0, 1)) : lesson.hour.substring(0, 2));
-			lesson.minutes = element.startTime.toString();
-			lesson.minutes = lesson.minutes.substring(lesson.minutes.length - 2);
+            // Validate the number of days
+            if (student.examsDays < 1 || student.examsDays > 360 || isNaN(student.examsDays)) { student.examsDays = 30; }
 
-			// Parse lesson number by start time
-			lesson.lessonNumber = startTimes[element.startTime];
+            var rangeStart = new Date(Date.now());
+            var rangeEnd = new Date(Date.now());
+            rangeEnd.setDate(rangeStart.getDate() + student.examsDays);
 
-			// Parse data about teacher
-			if (element.te) {
-				lesson.teacher = element.te[0].longname;
-				lesson.teacherInitial = element.te[0].name;
-			}
-			else {
-				lesson.teacher = "";
-				lesson.teacherInitial = "";
-			}
+            try {
+                //let exams;
+                exams = await untis.getExamsForRange(rangeStart, rangeEnd);
+                exams = this.examsToFlat(exams);
 
-			// Parse data about subject
-			if (element.su[0]) {
-				lesson.subject = element.su[0].longname;
-				lesson.subjectShort = element.su[0].name;
-			}
-			else {
-				lesson.subject = "";
-				lesson.subjectShort = "";
-			}
+            } catch (error) {
+                console.log("ERROR for " + student.title + ": " + error.toString());
+            };
+        }
+    
 
-			// Parse other information
-			lesson.code = element.code ? element.code : "";
-			lesson.text = element.lstext ? element.lstext : "";
-			lesson.substText = element.substText ? element.substText : "";
+        this.sendSocketNotification("GOT_DATA", { title: student.title, lessons: lessons, exams: exams, id: identifier });
+},
 
-			// Set code to "info" if there is an "substText" from WebUntis to display it if configuration "showRegularLessons" is set to false
-			if (lesson.substText != "" && lesson.code == "") {
-				lesson.code = "info";
-			}
+    timetableToLessons: function (startTimes, timetable) {
+        var lessons = [];
 
-			// Create sort string
-			lesson.sortString = lesson.year + lesson.month + lesson.day + lesson.hour + lesson.minutes;
-			switch (lesson.code) {
-				case "cancelled": lesson.sortString += "1"; break;
-				case "irregular": lesson.sortString += "2"; break;
-				case "info": lesson.sortString += "3"; break;
-				default: lesson.sortString += "9";
-			}
+        timetable.forEach(element => {
+            let lesson = {
+                year: element.date.toString().substring(0, 4),
+                month: element.date.toString().substring(4, 6),
+                day: element.date.toString().substring(6, 8),
+                hour: element.startTime.toString().padStart(4, "0").substring(0, 2),
+                minutes: element.startTime.toString().padStart(4, "0").substring(2),
+                startTime: element.startTime.toString().padStart(4, "0"),
+                teacher: element.te[0]?.longname || "N/A",
+                teacherInitial: element.te[0]?.name || "N/A",
+                subject: element.su[0]?.longname || "N/A",
+                subjectShort: element.su[0]?.name || "N/A",
+                code: element.code || "",
+                text: element.lstext || "",
+                substText: element.substText || "",
+                lessonNumber: startTimes[element.startTime],
+            };
 
-			lessons.push(lesson);
-		});
+            // Set code to "info" if there is an "substText" from WebUntis to display it if configuration "showRegularLessons" is set to false
+            if (lesson.substText != "" && lesson.code == "") {
+                lesson.code = "info";
+            }
 
-		if (this.config.debug) {
-			console.log("MMM-Webuntis: Timetable and Lessons: ", JSON.stringify({ timetable: timetable, lessons: lessons }));
-		}
+            // Create sort string
+            lesson.sortString = lesson.year + lesson.month + lesson.day + lesson.hour + lesson.minutes;
+            switch (lesson.code) {
+                case "cancelled": lesson.sortString += "1"; break;
+                case "irregular": lesson.sortString += "2"; break;
+                case "info": lesson.sortString += "3"; break;
+                default: lesson.sortString += "9";
+            }
 
-		return lessons;
-	},
+            lessons.push(lesson);
+        });
+        return lessons;
+    },
 
+    examsToFlat: function (exams) {
+        var ret_exams = [];
+
+        exams.forEach(element => {
+            let exam = {
+                year: element.examDate.toString().substring(0, 4),
+                month: element.examDate.toString().substring(4, 6),
+                day: element.examDate.toString().substring(6, 8),
+                startTime: element.startTime.toString().padStart(4, "0"),
+                teacher: element.teachers[0] || "N/A",
+                subject: element.subject || "N/A",
+                name: element.name || "",
+                text: element.text || "",
+                sortString: ""
+            };
+
+            // Create sort string
+            exam.sortString = exam.year + exam.month + exam.day;
+
+            ret_exams.push(exam);
+        });
+        return ret_exams;
+    }
 })
