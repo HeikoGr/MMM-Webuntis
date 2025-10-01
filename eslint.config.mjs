bb74edd383abd1d5993216df74ba24ef09d476ef
HeikoGr <20295490+HeikoGr@users.noreply.github.com>
2025-10-01 17:43:51 +0000

    chore: update devDependencies in package.json and add .sync_state file
    - Updated ESLint and related packages to newer versions for improved linting support.
    - Added new dependencies including cspell, husky, lint-staged, and prettier for enhanced code quality and formatting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,49 @@
+import css from "@eslint/css";
+import {defineConfig} from "eslint/config";
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import {flatConfigs as importX} from "eslint-plugin-import-x";
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
+import stylistic from "@stylistic/eslint-plugin";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default defineConfig([
+  {
+    files: ["**/*.css"],
+    plugins: {css},
+    language: "css/css",
+    extends: ["css/recommended"],
+    rules: {
+      "css/no-invalid-properties": "off"
+    }
+  },
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        Log: "readonly",
+        Module: "readonly"
+      }
+    },
+    plugins: {js, stylistic},
+    extends: [importX.recommended, "js/all", "stylistic/all"],
+    rules: {
+      "@stylistic/array-element-newline": ["error", "consistent"],
+      "@stylistic/dot-location": ["error", "property"],
+      "@stylistic/function-call-argument-newline": ["error", "consistent"],
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/object-property-newline": ["error", {allowAllPropertiesOnSameLine: true}],
+      "@stylistic/padded-blocks": ["error", "never"],
+      "@stylistic/quote-props": ["error", "as-needed"],
+      "capitalized-comments": "off",
+      "max-statements": ["error", 50],
+      "no-inline-comments": "off",
+      "no-magic-numbers": "off",
+      "one-var": ["error", "never"],
+      "sort-keys": "off"
+    }
+  },
+  {files: ["**/*.md"], plugins: {markdown}, language: "markdown/gfm", extends: ["markdown/recommended"]}
+]);
