8ce785591b312d267b82aa541d2dbd93369ddd17
Heiko Gr <20295490+HeikoGr@users.noreply.github.com>
2025-11-20 16:56:56 +0000

    fix: update lint script to ensure eslint runs before prettier and remove unused eslint dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,70 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import pluginN from "eslint-plugin-n";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    ignores: ["**/node_modules/**", "**/*.png"]
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    ignores: ["MMM-Webuntis.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node
+      }
+    },
+    plugins: {
+      n: pluginN
+    },
+    rules: {
+      ...pluginN.configs.recommended.rules
+    }
+  },
+  {
+    files: ["MMM-Webuntis.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        Log: "readonly",
+        Module: "readonly",
+        config: "readonly"
+      }
+    },
+    rules: {
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"]
+        }
+      ]
+    }
+  },
+  {
+    files: ["**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.mocha
+      }
+    }
+  }
 ]);
